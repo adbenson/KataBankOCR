22f2c688cf316a0967009441597aa132ab21cf83
Andrew D Benson <drew@adbenson.net>
2013-01-20 23:55:40 -0500

    Added test case to validate isValid method

--- src/net/adbenson/codekata/test/model/AccountNumberTestCase.java
+++ src/net/adbenson/codekata/test/model/AccountNumberTestCase.java
@@ -1,7 +1,10 @@
 package net.adbenson.codekata.test.model;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,5 +46,56 @@ public class AccountNumberTestCase {
 		
 		assertEquals(1234567890l, account.getValue());
 		assertEquals(digitList, account.getDigits());
+	}
+	
+	@Test
+	public void testValid() {
+		AccountNumber acct1 = mockNumber(123456789l);
+		assertTrue(acct1.isValid());
+		
+		AccountNumber acct2 = mockNumber(457508000l);
+		assertTrue(acct2.isValid());
+		
+		AccountNumber acct3 = mockNumber(000000000l);
+		assertTrue(acct3.isValid());
+		
+		AccountNumber acct4 = mockNumber(345882865l);
+		assertTrue(acct4.isValid());
+	}
+	
+	@Test
+	public void testInvalid() {
+		AccountNumber acct1 = mockNumber(664371495l);
+		assertFalse(acct1.isValid());
+		
+		AccountNumber acct2 = mockNumber(999999999l);
+		assertFalse(acct2.isValid());
+		
+		AccountNumber acct3 = mockNumber(111111111l);
+		assertFalse(acct3.isValid());
+		
+		AccountNumber acct4 = mockNumber(987654321l);
+		assertFalse(acct4.isValid());
+	}
+
+	private AccountNumber mockNumber(long value) {
+		List<Digit> digits = new ArrayList<Digit>();
+		
+		String number = new DecimalFormat("000000000").format(value);
+		for (int i=0; i<9; i++) {
+			int val = Integer.valueOf(number.charAt(i)+"");//String concat forces 'char' to String
+			digits.add(new MockDigit(val));
+		}
+		return new AccountNumber(digits, value);
+	}
+	
+	@Test
+	public void mockNumberMetaTest() {
+		AccountNumber asc = mockNumber(123456789l);
+		
+		assertEquals(1, asc.getDigits().get(0).getValue());
+		assertEquals(2, asc.getDigits().get(1).getValue());
+		assertEquals(5, asc.getDigits().get(4).getValue());
+		assertEquals(9, asc.getDigits().get(8).getValue());
 	}
 }
