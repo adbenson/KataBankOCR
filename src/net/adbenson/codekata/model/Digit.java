b599f52b3e797ec071ac1f5b0e9dfaf0f1f91c9c
Andrew D Benson <drew@adbenson.net>
2013-01-20 20:47:14 -0500

    Moving flatten method out of Digit class

--- src/net/adbenson/codekata/model/Digit.java
+++ src/net/adbenson/codekata/model/Digit.java
@@ -1,9 +1,6 @@
 package net.adbenson.codekata.model;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import net.adbenson.codekata.common.Config;
 
 /**
  * Digit class models a "graphical" digit as a set of String rows
@@ -27,18 +24,6 @@ public class Digit {
 		this.value = value;
 	}
 	
-	/**
-	 * Flattens the 2D representation of this character to a simple string for pattern matching
-	 * @return a simple string representation of this digit for pattern matching
-	 */
-	public String flatten() {
-		StringBuilder flat = new StringBuilder();
-		for (String row : rows) {
-			flat.append(row);
-		}
-		return flat.toString();
-	}
-
 	public Object getRows() {
 		return rows;
 	}
--- src/net/adbenson/codekata/parser/DigitParser.java
+++ src/net/adbenson/codekata/parser/DigitParser.java
@@ -23,12 +23,26 @@ public class DigitParser {
 	 * @param digit
 	 * @return the Digit corresponding to the given symbol
 	 */
-	public Digit parse(List<String> digitLines) {		
+	public Digit parse(List<String> digitLines) {
+		
+		Digit digit = new Digit(digitLines);
 		return digitPatterns.get(digit.flatten());
 	}
 
 	public void setDigitPatterns(HashMap<String, Integer> digitPatterns) {
 		this.digitPatterns = digitPatterns;
+	}
+	
+	/**
+	 * Flattens the 2D representation of this character to a simple string for pattern matching
+	 * @return a simple string representation of this digit for pattern matching
+	 */
+	private String flatten(List<String> rows) {
+		StringBuilder flat = new StringBuilder();
+		for (String row : rows) {
+			flat.append(row);
+		}
+		return flat.toString();
 	}
 
 }
